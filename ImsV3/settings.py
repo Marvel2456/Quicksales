6b0c706adca844894bd599d2abb78f1062c00885
Marvel2456 <marvellousidemudia@gmail.com>
2022-10-29 07:34:08 +0100

    deploy to railway

--- ImsV3/settings.py
+++ ImsV3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#hwy0&m5td1l*^7i3(lu$wdl*4)shrhz$%ycp=qdx8tg(#fpu%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
